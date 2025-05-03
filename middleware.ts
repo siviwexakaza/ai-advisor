6d7156a10d4863e69ab39f095328b33fc58b4184
sivnodlela <152187964+siviwe-asthat@users.noreply.github.com>
2025-05-03 16:48:16 +0200

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/"]);
+const isPublicRoute = createRouteMatcher(["/", "/api/vapi/new"]);
 
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
